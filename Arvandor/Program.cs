a63a90355506816fccc4ff32a739255cfa059c27
MartinAgarcia1710 <118487586+MartinAgarcia1710@users.noreply.github.com>
2024-06-16 12:54:03 -0300

    Titulo
    titulo posicionado y versión del juego con tipografía

--- Arvandor/Program.cs
+++ Arvandor/Program.cs
@@ -11,21 +11,53 @@ namespace Arvandor
     {
         static void Main(string[] args)
         {
-            Console.WindowHeight = 30;
-            Console.WindowWidth = 73;
+            Console.CursorVisible = false;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            
+            int TitlePositionX = 25;
+            int TitlePositionY = 5;
             int op;
             GamePlay game = new GamePlay();
-            Console.BackgroundColor = ConsoleColor.Gray;
+            
+            Console.SetCursorPosition(TitlePositionX, TitlePositionY);
+            Console.WriteLine("░█████╗░ ██████╗░ ██╗░░░██╗ ░█████╗░ ███╗░░██╗ ██████╗░ ░█████╗░ ██████╗░");
+            Console.SetCursorPosition(TitlePositionX, TitlePositionY + 1);
+            Console.WriteLine("██╔══██╗ ██╔══██╗ ██║░░░██║ ██╔══██╗ ████╗░██║ ██╔══██╗ ██╔══██╗ ██╔══██╗");
+            Console.SetCursorPosition(TitlePositionX, TitlePositionY + 2);
+            Console.WriteLine("███████║ ██████╔╝ ╚██╗░██╔╝ ███████║ ██╔██╗██║ ██║░░██║ ██║░░██║ ██████╔╝");
+            Console.SetCursorPosition(TitlePositionX, TitlePositionY + 3);
+            Console.WriteLine("██╔══██║ ██╔══██╗ ░╚████╔╝░ ██╔══██║ ██║╚████║ ██║░░██║ ██║░░██║ ██╔══██╗");
+            Console.SetCursorPosition(TitlePositionX, TitlePositionY + 4);
+            Console.WriteLine("██║░░██║ ██║░░██║ ░░╚██╔╝░░ ██║░░██║ ██║░╚███║ ██████╔╝ ╚█████╔╝ ██║░░██║");
+            Console.SetCursorPosition(TitlePositionX, TitlePositionY + 5);
+            Console.WriteLine("╚═╝░░╚═╝ ╚═╝░░╚═╝ ░░░╚═╝░░░ ╚═╝░░╚═╝ ╚═╝░░╚══╝ ╚═════╝░ ░╚════╝░ ╚═╝░░╚═╝");
+            Console.SetCursorPosition(TitlePositionX - 10, TitlePositionY + 7);
+            Console.WriteLine("  ___              _                    _  _    _     _       _____                    _   ");
+            Console.SetCursorPosition(TitlePositionX - 10, TitlePositionY + 8);
+            Console.WriteLine(" / _ \\            | |                  | || |  | |   ( )     |  _  |                  | | ");
+            Console.SetCursorPosition(TitlePositionX - 10, TitlePositionY + 9);
+            Console.WriteLine("/ /_\\ \\ _ __  ___ | |__    __ _   __ _ | || |_ | |__ |/ ___  | | | | _   _   ___  ___ | |_");
+            Console.SetCursorPosition(TitlePositionX - 10, TitlePositionY + 10);
+            Console.WriteLine("|  _  || '__|/ __|| '_ \\  / _` | / _` || || __|| '_ \\  / __| | | | || | | | / _ \\/ __|| __|");
+            Console.SetCursorPosition(TitlePositionX - 10, TitlePositionY + 11);
+            Console.WriteLine("| | | || |   \\__ \\| | | || (_| || (_| || || |_ | | | | \\__ \\ \\ \\/' /| |_| ||  __/\\__ \\| |_ ");
+            Console.SetCursorPosition(TitlePositionX - 10, TitlePositionY + 12);
+            Console.WriteLine("\\_| |_/|_|   |___/|_| |_| \\__,_| \\__,_||_| \\__||_| |_| |___/  \\_/\\_\\ \\__,_| \\___||___/ \\__|");
+            Console.SetCursorPosition(TitlePositionX - 8, TitlePositionY + 13);
+            Console.WriteLine("                                                                                           ");
+            Console.SetCursorPosition(TitlePositionX - 5, TitlePositionY + 14);
+            
+            Console.SetCursorPosition(TitlePositionX + 30, TitlePositionY + 16);
+            Console.WriteLine("Push a button");
+           
+            Console.ReadKey();
             Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.CursorVisible = false;
-            
-            Console.WriteLine("░█████╗░ ██████╗░ ██╗░░░██╗ ░█████╗░ ███╗░░██╗ ██████╗░ ░█████╗░ ██████╗░\r\n██╔══██╗ ██╔══██╗ ██║░░░██║ ██╔══██╗ ████╗░██║ ██╔══██╗ ██╔══██╗ ██╔══██╗\r\n███████║ ██████╔╝ ╚██╗░██╔╝ ███████║ ██╔██╗██║ ██║░░██║ ██║░░██║ ██████╔╝\r\n██╔══██║ ██╔══██╗ ░╚████╔╝░ ██╔══██║ ██║╚████║ ██║░░██║ ██║░░██║ ██╔══██╗\r\n██║░░██║ ██║░░██║ ░░╚██╔╝░░ ██║░░██║ ██║░╚███║ ██████╔╝ ╚█████╔╝ ██║░░██║\r\n╚═╝░░╚═╝ ╚═╝░░╚═╝ ░░░╚═╝░░░ ╚═╝░░╚═╝ ╚═╝░░╚══╝ ╚═════╝░ ░╚════╝░ ╚═╝░░╚═╝");
 
-            
 
-            
-            Console.WriteLine("\t    Arshaalth's Quest");
+
+
 
             Console.WriteLine("╔■═■══════════╗\r\n║   1. Play   ║\n║   2. Help   ║\n║   3. Load   ║\n║   4. Quit   ║\r\n╚══════════■═■╝");
             Console.WriteLine("1. Play\n2. Help\n3. Load\n4. Quit");
