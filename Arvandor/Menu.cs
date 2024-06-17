1b77308bf6d0feb2be0feeab8582d6d9667d1cea
MartinAgarcia1710 <118487586+MartinAgarcia1710@users.noreply.github.com>
2024-06-16 22:53:53 -0300

    Menu seleccionable
    Primera versión del menú seleccionable con teclas. No me convence, tiene algunas fallas. Por otro lado, hay que posicionar bien en la consola. Hay que seguir viendo los colores de la consola

---
+++ Arvandor/Menu.cs
@@ -0,0 +1,86 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvandor
+{
+    public static class Menu
+    {
+        public static string selection(string[] list, int op, int x, int y)
+        {
+            string select = string.Empty;
+            int dest = 0;
+
+            
+
+                    
+            for (int z = 0; z < list.Length; z++)
+            {
+ 
+                if (dest == op)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    
+                    Console.WriteLine(list[z]);
+                    //Console.ResetColor();
+                    select = list[z];
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.CursorLeft = 0;
+                    
+                    Console.WriteLine(list[z]);
+
+                }
+                dest++;
+            }
+
+            return select;
+        }
+
+
+
+
+
+        /*
+        public static string selection(string[] list, int op, int x, int y)
+        {
+            string select = string.Empty;
+            int dest = 0;
+            
+                
+            for (int z = 0; z < list.Length; z++)
+            {
+                if (dest == op)
+                {
+                    
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(list[z]);
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    select = list[z];
+                }
+                else
+                {
+                    
+                    Console.WriteLine(new string(' ', Console.WindowWidth));
+                    Console.CursorLeft = 0;
+                    Console.WriteLine(list[z]);
+                }
+                    dest++;
+            }
+
+            
+            return select;
+
+        }*/
+    }
+}
--- Arvandor/Program.cs
+++ Arvandor/Program.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Spectre.Console;
 namespace Arvandor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+           
+
+           
+
+
+
+
+
+            int x;
+            int y;
+            int counter = 0;
+            bool loop = true;
+            ConsoleKeyInfo k;
+
+            string[] principalOptions = new string[]
+            {
+                "Play",
+                "Help",
+                "Load",
+                "Quit"
+            };
             Console.CursorVisible = false;
             Console.BackgroundColor = ConsoleColor.DarkGreen;
             Console.ForegroundColor = ConsoleColor.Black;
@@ -56,11 +78,52 @@ namespace Arvandor
             Console.Clear();
 
 
+            x = Console.CursorLeft; 
+            y = Console.CursorTop;
+            int prueba = 0;
+            string selOp = Menu.selection(principalOptions, counter, 30, 10);
+            while (loop)
+            {
+                while ((k = Console.ReadKey(true)).Key != ConsoleKey.Enter)
+                {
+                    switch(k.Key)
+                    {
+                        case ConsoleKey.DownArrow:
+                            if (counter == principalOptions.Length - 1)
+                            {
+                                continue;
+                            }
+                            counter++;
+                            
+                            break;
+                        case ConsoleKey.UpArrow: 
+                            if(counter == 0)
+                            {
+                                continue;
+                            }
+                            counter--;
+                            break;
+                        case ConsoleKey.Enter:
+                            game.selectCharacter();
+                            break;
+                        default: 
+                            break;
+                    }
 
+                    Console.CursorLeft = x; 
+                    Console.CursorTop = y;
+            
+                    selOp = Menu.selection(principalOptions, counter, 30, 10);
+                }
+            }
 
-
-            Console.WriteLine("╔■═■══════════╗\r\n║   1. Play   ║\n║   2. Help   ║\n║   3. Load   ║\n║   4. Quit   ║\r\n╚══════════■═■╝");
-            Console.WriteLine("1. Play\n2. Help\n3. Load\n4. Quit");
+            //Console.WriteLine("╔■═■══════════╗\r\n║   1. Play   ║\n║   2. Help   ║\n║   3. Load   ║\n║   4. Quit   ║\r\n╚══════════■═■╝");
+            
+            
+            
+            
+            
+            //Console.WriteLine("1. Play\n2. Help\n3. Load\n4. Quit");
             op = int.Parse(Console.ReadLine());
             while(true)
             {
